25de82df5811ff51bd45b989c97eccfc59ebe604
shoaib <p200147@pwr.nu.edu.pk>
2021-09-17 13:00:56 +0500

    Adding Call by value with pointers file

---
+++ Lab-Work/Lab-Work-1/call_by_value_with_pointers.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+void call_by_value(int *myInt)
+{
+
+
+    *myInt = *myInt +1;
+
+    cout<<"Value of my int is: "<<*myInt<<endl;
+
+
+}
+
+int main()
+{
+
+    int a = 10;
+
+    int *ptr = &a;
+
+    cout<<"Value of a before calling the function is: "<<a<<endl;
+
+    call_by_value(ptr); // Now here we can also give  "&a" because ptr has the address of  "a" and we can give this address directly...
+
+    cout<<"Value of a after calling the function is: "<<a<<endl;
+
+
+
+
+}
