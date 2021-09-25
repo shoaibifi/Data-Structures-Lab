a63d6a630b3e0e47b2966fb2f409274931c3732f
shoaibifi <p200147@pwr.nu.edu.pk>
2021-09-25 13:08:10 +0500

    Adding the constant_pointers_and_variables_2 file

---
+++ Lab-Work/Lab-Work-2/constant_pointer_and_variable_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+
+    int x = 10;
+
+    const int * p2a = &x; // non-const pointer to const int
+    int const * p2b = &x; // also non-const pointer to const int
+
+
+
+
+}
